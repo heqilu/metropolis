d3e6296e87ba5448524892d0455b1f6acfcbbb21
Heqi LU <heqi.lu.code@gmail.com>
2017-04-23 14:35:17 +0200

    common types and flatbuffer helloworld

---
+++ EveTrex/include/common.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace EveTrex {
+
+typedef int int32;
+typedef unsigned int uint32;
+typedef __int64 int64;
+typedef unsigned __int64 uint64;
+typedef short int16;
+typedef unsigned short uint16;
+typedef char int8;
+typedef unsigned char uint8;
+
+}
